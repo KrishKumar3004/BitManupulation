99a37c19f9d07cb180145d0df398e748196e1a6d
Krish <krishkumar3004@gmail.com>
2022-10-07 10:10:28 +0530

    Second Commit

--- evenOrOdd.cpp
+++ evenOrOdd.cpp
@@ -1,11 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
-    if(n&1==1) cout<<"ODD";
-    else cout<<"EVEN";
-   return 0;
+    cin >> n;
+    if (n & 1 == 1)
+        cout << "ODD";
+    else
+        cout << "EVEN";
+    return 0;
+    // This comment has no purpose. I am just learning git.
 }
